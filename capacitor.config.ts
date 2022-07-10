30770cee3e84192565f4bf4c7e95af45acc60825
Keagan <keagan@artfin.com>
2022-07-10 09:11:31 -0500

    IOS/Android App

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mynuaxess.www',
+  appName: 'NuAxess Employer',
+  webDir: 'dist/fuse',
+  bundledWebRuntime: false
+};
+
+export default config;
